afe3e28543df2986aba62127db1def9291f76dde
qorzj <zhangji@parsec.com.cn>
2018-08-13 10:03:17 +0800

    修改deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
-tar czf lessweb_org.tar.gz start.py build.sh lessweb_org/ data/ static/ test/
+mkdir -p dist && rm -rf dist/*
+tar czf dist/lessweb_org.tar.gz start.py lessweb_org/ data/ static/ test/
 docker-compose -p armstrong -f docker/production/docker-compose.yml up -d --build
